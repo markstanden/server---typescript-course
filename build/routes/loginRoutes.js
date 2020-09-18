f86ddc75063bc55e8fb3c77fdf8ff850ce45963d
Mark Standen <markstanden80@icloud.com>
2020-09-18 22:44:24 +0100

    type guarding on the response

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -5,9 +5,16 @@ var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (request, response) {
-    response.send("\n    <form method = \"POST\" >\n    <div>\n      <label>Email</label>\n      <input name=\"email\" />\n    </div>\n    <div>\n      <label>Password</label>\n      <input name=\"password\" type=\"password\" />\n    </div>\n    <button>Submit</button>\n\n  ");
+    response.send("\n    <form method = \"POST\" >\n    <div>\n      <label>Email</label>\n      <input name=\"email\" />\n    </div>\n    <div>\n      <label>Password</label>\n      <input name=\"password\" type=\"password\" />\n    </div>\n    <button>Submit</button>\n  ");
     router.post('/login', function (request, response) {
         var _a = request.body, email = _a.email, password = _a.password;
-        response.send(email + password);
+        if (email && password && email === 'hi@hi.com' && password === 'password') {
+            response.send('ok');
+            //mark as signed in
+            //redirect them to the root route
+        }
+        else {
+            response.send('Invalid email or Password');
+        }
     });
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/login', (request: Request, response: Response) => {
@@ -14,13 +18,18 @@ router.get('/login', (request: Request, response: Response) => {
       <input name="password" type="password" />
     </div>
     <button>Submit</button>
-
   `);
 
-  router.post('/login', (request: Request, response: Response) => {
+  router.post('/login', (request: RequestWithBody, response: Response) => {
     const { email, password } = request.body;
 
-    response.send(email + password);
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+      response.send('ok');
+      //mark as signed in
+      //redirect them to the root route
+    } else {
+      response.send('Invalid email or Password');
+    }
   });
 });
 
