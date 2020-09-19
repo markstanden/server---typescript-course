c44080d2cfba086b426c35d2321773dd22d0b663
Mark Standen <markstanden80@icloud.com>
2020-09-19 21:55:06 +0100

    added (very) basic cookie authentication, fixed a bug (dependancy changed types) thanks to typescript within seconds

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -8,13 +8,26 @@ router.get('/login', function (request, response) {
     response.send("\n    <form method = \"POST\" >\n    <div>\n      <label>Email</label>\n      <input name=\"email\" />\n    </div>\n    <div>\n      <label>Password</label>\n      <input name=\"password\" type=\"password\" />\n    </div>\n    <button>Submit</button>\n  ");
     router.post('/login', function (request, response) {
         var _a = request.body, email = _a.email, password = _a.password;
-        if (email && password && email === 'hi@hi.com' && password === 'password') {
-            response.send('ok');
+        if (email && password && email === 'a@b.com' && password === 'c') {
             //mark as signed in
+            request.session = { loggedIn: true };
             //redirect them to the root route
+            response.redirect('/');
         }
         else {
             response.send('Invalid email or Password');
         }
     });
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n    <div>\n      <div> You Are Logged In </div>\n      <a href=\"/logout\">Logout</a>\n    </div>\n    ");
+    }
+    else {
+        res.send("\n    <div>\n      <div> You Are NOT Logged In </div>\n      <a href=\"/login\">Login here</a>\n    </div>\n    ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = null;
+    res.redirect('/');
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -23,14 +23,39 @@ router.get('/login', (request: Request, response: Response) => {
   router.post('/login', (request: RequestWithBody, response: Response) => {
     const { email, password } = request.body;
 
-    if (email && password && email === 'hi@hi.com' && password === 'password') {
-      response.send('ok');
+    if (email && password && email === 'a@b.com' && password === 'c') {
       //mark as signed in
+      request.session = { loggedIn: true };
+
       //redirect them to the root route
+      response.redirect('/');
     } else {
       response.send('Invalid email or Password');
     }
   });
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <div> You Are Logged In </div>
+      <a href="/logout">Logout</a>
+    </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div> You Are NOT Logged In </div>
+      <a href="/login">Login here</a>
+    </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect('/');
 });
 
 export { router };
